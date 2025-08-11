53b75c756745eeaf6f678f98f25868a4ac5e2ce7
HJ-Chinthan7 <chinthanhj107@gmail.com>
2025-08-11 21:45:41 +0530

    mongo model

--- models/url.js
+++ models/url.js
@@ -1,1 +1,24 @@
-const 
+const mongoose=require("mongoose");
+const urlSchema=new mongoose.Schema({
+    originalUrl:{
+        type:String,
+        required:true
+    },
+    shortUrl:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    urlCode:{
+        type:String,
+        required:true,
+        unique:true
+    }
+    ,
+    clicks:{
+        type:Number,
+        default:0
+    }
+},{timestamps});
+
+module.exports=mongoose.model('Url',urlSchema);
